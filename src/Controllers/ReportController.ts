6e72f6041171e85ac5497151a9b3ffa40a65288f
Marty <marsmith@usgs.gov>
2016-01-28 16:31:36 -0500

    experiment with jsPDF, left stub code in

--- src/Controllers/ReportController.ts
+++ src/Controllers/ReportController.ts
@@ -24,6 +24,9 @@
 
 //Imports"
 module StreamStats.Controllers {
+
+    declare var jsPDF;
+
     'use strinct';
     interface IReportControllerScope extends ng.IScope {
         vm: ReportController;
@@ -273,6 +276,50 @@ module StreamStats.Controllers {
                 }
             }
 
+        }
+
+        private downloadPDF() {
+            var pdf = new jsPDF('p', 'pt', 'letter');
+            // source can be HTML-formatted string, or a reference
+            // to an actual DOM element from which the text will be scraped.
+
+            //var source = $('#customers')[0];
+            //var source = angular.element(document.getElementById('printArea'));
+            var source = document.getElementById('printArea').innerHTML;
+            console.log(source);
+
+            // we support special element handlers. Register them with jQuery-style 
+            // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
+            // There is no support for any other type of selectors 
+            // (class, of compound) at this time.
+            var specialElementHandlers = {
+                // element with id of "bypass" - jQuery style selector
+                '#bypassme': function (element, renderer) {
+                    // true = "handled elsewhere, bypass text extraction"
+                    return true
+                }
+            };
+            var margins = {
+                top: 80,
+                bottom: 60,
+                left: 40,
+                width: 522
+            };
+            // all coords and widths are in jsPDF instance's declared units
+            // 'inches' in this case
+            pdf.fromHTML(
+                source, // HTML string or DOM elem ref.
+                margins.left, // x coord
+                margins.top, { // y coord
+                    'width': margins.width, // max width of content on PDF
+                    'elementHandlers': specialElementHandlers
+                },
+
+                function (dispose) {
+                    // dispose: object with X, Y of the last line add to the PDF 
+                    //          this allow the insertion of new lines after html
+                    pdf.save('Test.pdf');
+                }, margins);
         }
 
         //Helper Methods
