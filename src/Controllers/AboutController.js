4d52157009d940131c501eedf9d92a841065d062
Marty <marsmith@usgs.gov>
2016-03-18 14:54:50 -0400

    remove dismiss button after news item has been dismissed

--- src/Controllers/AboutController.js
+++ src/Controllers/AboutController.js
@@ -143,6 +143,18 @@ var StreamStats;
                 this.getActiveNews();
                 this.getPastNews();
             };
+            AboutController.prototype.readCookie = function (name) {
+                var nameEQ = name + "=";
+                var ca = document.cookie.split(';');
+                for (var i = 0; i < ca.length; i++) {
+                    var c = ca[i];
+                    while (c.charAt(0) == ' ')
+                        c = c.substring(1, c.length);
+                    if (c.indexOf(nameEQ) == 0)
+                        return c.substring(nameEQ.length, c.length);
+                }
+                return null;
+            };
             AboutController.prototype.createCookie = function (name, value, days) {
                 if (days) {
                     var date = new Date();
