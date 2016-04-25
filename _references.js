a19132d3fa2e3e9f553ecf1afb726a768c79f7ac
jknewson <jknewson@usgs.gov>
2016-04-25 16:31:06 -0600

    fixed some issues

---
+++ _references.js
@@ -0,0 +1,15 @@
+/// <reference path="typings/main.d.ts" />
+/// <reference path="bower_components/wim_angular/dist/typings/Extensions/String.d.ts" />
+/// <reference path="bower_components/wim_angular/dist/typings/Extensions/SurveyRound.d.ts" />
+/// <reference path="bower_components/wim_angular/dist/typings/Models/Point.d.ts" />
+/// <reference path="bower_components/wim_angular/dist/typings/Models/Parameter.d.ts" />
+/// <reference path="bower_components/wim_angular/dist/typings/Services/Helpers/RequestInfo.d.ts" />
+/// <reference path="bower_components/wim_angular/dist/typings/Services/HTTPServiceBase.d.ts" />
+/// <reference path="bower_components/wim_angular/dist/typings/Services/SearchAPIService.d.ts" />
+/// <reference path="bower_components/wim_angular/dist/typings/Events/Delegate.d.ts" />
+/// <reference path="bower_components/wim_angular/dist/typings/Events/EventArgs.d.ts" />
+/// <reference path="bower_components/wim_angular/dist/typings/Events/EventHandler.d.ts" />
+/// <reference path="bower_components/wim_angular/dist/typings/Services/Helpers/RequestTransform.d.ts" />
+/// <reference path="bower_components/wim_angular/dist/typings/Directives/wimlegend.d.ts" />
+/// <reference path="bower_components/wim_angular/dist/typings/Events/eventmanager.d.ts" /> 
+//# sourceMappingURL=_references.js.map
