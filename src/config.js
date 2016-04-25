d8d429326c71ff832de111ab2eb302010f16bc01
jknewson <jknewson@usgs.gov>
2016-04-25 16:34:52 -0600

    commit

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //http://lgorithms.blogspot.com/2013/07/angularui-router-as-infrastructure-of.html
 //http://www.funnyant.com/angularjs-ui-router/
 var StreamStats;
@@ -55,62 +54,4 @@ var StreamStats;
     ])
         .config(config);
 })(StreamStats || (StreamStats = {})); //end module 
-=======
-//http://lgorithms.blogspot.com/2013/07/angularui-router-as-infrastructure-of.html
-//http://www.funnyant.com/angularjs-ui-router/
-var StreamStats;
-(function (StreamStats) {
-    //'use strict';
-    var config = (function () {
-        function config($stateProvider, $urlRouterProvider, $locationProvider, $logProvider) {
-            this.$stateProvider = $stateProvider;
-            this.$urlRouterProvider = $urlRouterProvider;
-            this.$locationProvider = $locationProvider;
-            this.$logProvider = $logProvider;
-            this.$stateProvider
-                .state("main", {
-                url: '/?rcode&workspaceID',
-                //reloadOnSearch:true,
-                template: '<ui-view/>',
-                views: {
-                    'map': {
-                        templateUrl: "Views/mapview.html",
-                        controller: "StreamStats.Controllers.MapController"
-                    },
-                    'sidebar': {
-                        templateUrl: "Views/sidebarview.html",
-                        //abstract:true,
-                        controller: "StreamStats.Controllers.SidebarController"
-                    },
-                    'navbar': {
-                        templateUrl: "Views/navigationview.html",
-                        controller: "StreamStats.Controllers.NavbarController"
-                    },
-                    'report': {
-                        templateUrl: "Views/reportview.html",
-                        controller: "StreamStats.Controllers.ReportController"
-                    }
-                }
-            }); //end main state 
-            this.$urlRouterProvider.otherwise('/');
-            this.$locationProvider.html5Mode(true);
-            //turns of angular-leaflet console spam
-            this.$logProvider.debugEnabled(false);
-        } //end constructor
-        config.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider', '$logProvider'];
-        return config;
-    }()); //end class
-    angular.module('StreamStats', [
-        'ui.router', 'ui.bootstrap', 'ui.checkbox',
-        'mobile-angular-ui',
-        'angulartics', 'angulartics.google.analytics',
-        'toaster', 'ngAnimate', 'ngFileUpload',
-        'leaflet-directive',
-        'StreamStats.Services',
-        'StreamStats.Controllers',
-        'WiM.Services', 'WiM.Event', 'wim_angular', 'rzModule', 'nvd3'
-    ])
-        .config(config);
-})(StreamStats || (StreamStats = {})); //end module 
->>>>>>> 3fbfe1fe657d77c9a55950f17e25ea44d89caa5d
 //# sourceMappingURL=config.js.map
