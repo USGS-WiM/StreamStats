e03c1f37802fbb427c6fa9e856f0c7ff40e81eed
jknewson <jknewson@usgs.gov>
2016-04-25 16:34:52 -0600

    commit

--- src/Controllers/controller.module.js
+++ src/Controllers/controller.module.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 (function () {
     'use strict';
     angular
         .module('StreamStats.Controllers', []);
 })();
-=======
-(function () {
-    'use strict';
-    angular
-        .module('StreamStats.Controllers', []);
-})();
->>>>>>> 3fbfe1fe657d77c9a55950f17e25ea44d89caa5d
 //# sourceMappingURL=controller.module.js.map
--- src/Models/WatershedEDL.js
+++ src/Models/WatershedEDL.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //------------------------------------------------------------------------------
 //----- Point ------------------------------------------------------------------
 //------------------------------------------------------------------------------
@@ -31,38 +30,4 @@ var StreamStats;
         Models.WatershedEditDecisionList = WatershedEditDecisionList; //end class
     })(Models = StreamStats.Models || (StreamStats.Models = {}));
 })(StreamStats || (StreamStats = {})); //end module 
-=======
-//------------------------------------------------------------------------------
-//----- Point ------------------------------------------------------------------
-//------------------------------------------------------------------------------
-//-------1---------2---------3---------4---------5---------6---------7---------8
-//       01234567890123456789012345678901234567890123456789012345678901234567890
-//-------+---------+---------+---------+---------+---------+---------+---------+
-// copyright:   2014 WiM - USGS
-//    authors:  Jeremy K. Newson USGS Wisconsin Internet Mapping
-//             
-// 
-//   purpose:  
-//          
-//discussion:
-//
-//Comments
-//08.20.2014 jkn - Created
-//Imports"
-// Interface
-var StreamStats;
-(function (StreamStats) {
-    var Models;
-    (function (Models) {
-        var WatershedEditDecisionList = (function () {
-            function WatershedEditDecisionList() {
-                this.append = [];
-                this.remove = [];
-            }
-            return WatershedEditDecisionList;
-        }());
-        Models.WatershedEditDecisionList = WatershedEditDecisionList; //end class
-    })(Models = StreamStats.Models || (StreamStats.Models = {}));
-})(StreamStats || (StreamStats = {})); //end module 
->>>>>>> 3fbfe1fe657d77c9a55950f17e25ea44d89caa5d
 //# sourceMappingURL=WatershedEDL.js.map
