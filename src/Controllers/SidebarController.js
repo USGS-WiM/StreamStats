1cba46073d5cb88d9fa1d04238ec76a86e162c32
Marty <marsmith@usgs.gov>
2016-10-13 16:45:38 -0400

    fix colorado inconsistencies.  add support for basin char failure.  reorg some sidebar elements

--- src/Controllers/SidebarController.js
+++ src/Controllers/SidebarController.js
@@ -228,8 +228,11 @@ var StreamStats;
                     category: 'SideBar', label: this.regionService.selectedRegion.Name + '; ' + this.nssService.selectedStatisticsGroupList.map(function (elem) { return elem.Name; }).join(",") });
                 if (this.nssService.selectedStatisticsGroupList.length > 0 && this.nssService.showFlowsTable) {
                     this.nssService.estimateFlows(this.studyAreaService.studyAreaParameterList, "value", this.regionService.selectedRegion.RegionID, this.studyAreaService.selectedStudyArea.RegressionRegions.map(function (elem) { return elem.code; }).join(","));
-                    if (this.studyAreaService.Disclaimers["isRegulated"])
-                        this.nssService.estimateFlows(this.studyAreaService.studyAreaParameterList, "unRegulatedValue", this.regionService.selectedRegion.RegionID, this.studyAreaService.selectedStudyArea.RegressionRegions.map(function (elem) { return elem.code; }).join(","), true);
+                    if (this.studyAreaService.Disclaimers["isRegulated"]) {
+                        setTimeout(function () {
+                            _this.nssService.estimateFlows(_this.studyAreaService.studyAreaParameterList, "unRegulatedValue", _this.regionService.selectedRegion.RegionID, _this.studyAreaService.selectedStudyArea.RegressionRegions.map(function (elem) { return elem.code; }).join(","), true);
+                        }, 500);
+                    }
                 }
                 this.modalService.openModal(StreamStats.Services.SSModalType.e_report);
                 this.studyAreaService.reportGenerated = true;
