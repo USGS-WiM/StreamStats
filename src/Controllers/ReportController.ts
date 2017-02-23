a1ed689a44fcc4e364e50b2a36cdd8ff68933fb4
Marty <marsmith@usgs.gov>
2017-02-23 11:08:06 -0500

    upgrade all dependencies, clean up gulpfile

--- src/Controllers/ReportController.ts
+++ src/Controllers/ReportController.ts
@@ -242,7 +242,7 @@ module StreamStats.Controllers {
 
                 data.forEach((item) => {
                     if (this.studyAreaService.selectedStudyArea.Disclaimers['isRegulated']) finalVal += item.code + ',' + item.name + ',' + item.value + ',' + item.unRegulatedValue.toFixed(2) + ',' + item.regulatedValue.toFixed(2) + ',' + item.unit + '\n';
-                    else finalVal += item.code + ',' + item.name + ',' + item.value + ',' + item.unit + '\n';                   
+                    else finalVal += item.code + ',' + item.description + ',' + item.value + ',' + item.unit + '\n';                   
                 });
                 return finalVal + '\n';
             };
