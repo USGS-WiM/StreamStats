7f4e6e339cf959a7b73e18064f3dd1c7c1e64039
Marty <marsmith@usgs.gov>
2017-02-26 13:22:41 -0500

    implement new search API

--- src/Controllers/MapController.js
+++ src/Controllers/MapController.js
@@ -768,20 +768,8 @@ var StreamStats;
             MapController.prototype.onSelectedAreaOfInterestChanged = function (sender, e) {
                 //ga event
                 this.angulartics.eventTrack('Search', { category: 'Sidebar' });
-                //this.markers = {};
-                var AOI = e.selectedAreaOfInterest;
-                if (AOI.Category == "U.S. State or Territory")
-                    var zoomlevel = 9;
-                else
-                    var zoomlevel = 14;
-                //this.markers['AOI'] = {
-                //    lat: AOI.Latitude,
-                //    lng: AOI.Longitude,
-                //    message: AOI.Name,
-                //    focus: true,
-                //    draggable: true
-                //}
                 this.paths = {};
+                var AOI = e.selectedAreaOfInterest;
                 this.paths['AOI'] = {
                     type: "circleMarker",
                     radius: 15,
@@ -789,13 +777,19 @@ var StreamStats;
                     fillOpacity: 0.6,
                     stroke: false,
                     latlngs: {
-                        lat: AOI.Latitude,
-                        lng: AOI.Longitude,
+                        lat: AOI.properties['Lat'],
+                        lng: AOI.properties['Lon'],
                     }
                 };
-                //this.center = new Center(AOI.Latitude, AOI.Longitude, zoomlevel);
                 this.leafletData.getMap("mainMap").then(function (map) {
-                    map.setView([AOI.Latitude, AOI.Longitude], zoomlevel);
+                    map.fitBounds([
+                        [AOI.properties['LatMin'], AOI.properties['LonMin']],
+                        [AOI.properties['LatMax'], AOI.properties['LonMax']]
+                    ]);
+                    map.openPopup(// open popup at location listing all properties
+                    $.map(Object.keys(AOI.properties), function (property) {
+                        return "<b>" + property + ": </b>" + AOI.properties[property];
+                    }).join("<br/>"), [AOI.properties['Lat'], AOI.properties['Lon']]);
                 });
             };
             MapController.prototype.onSelectedRegionChanged = function () {
