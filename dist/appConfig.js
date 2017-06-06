445107b51bf27ad216e6c226933caf7676c9a711
Marty <marsmith@usgs.gov>
2017-06-06 12:14:28 -0400

    fix IE printing with conditional CSS, implement additional features

--- dist/appConfig.js
+++ dist/appConfig.js
@@ -27,6 +27,8 @@ configuration.queryparams =
     'SSeditBasin': '/streamstatsservices/watershed/edit.{0}?rcode={1}&workspaceID={2}&crs={3}&simplify=true&includeparameters=false&includeflowtypes=false&includefeatures=true',
     'SSAvailableParams': '/streamstatsservices/parameters.json?rcode={0}',
     'SSComputeParams': '/streamstatsservices/parameters.json?rcode={0}&workspaceID={1}&includeparameters={2}',
+    'SSavailableFeatures': '/streamstatsservices/features.json?workspaceID={0}',
+    'SSfeatures': '/streamstatsservices/features.geojson?workspaceID={0}&crs={1}&includefeatures={2}&simplify=true',
     'SSStateLayers': '/arcgis/rest/services/{0}_ss/MapServer?f=pjson',
     'SSNationalLayers': '/arcgis/rest/services/ss_studyAreas_prod/MapServer',
     'FARefGage': '/2/query?geometry={0}&geometryType=esriGeometryPoint&inSR={1}&spatialRel=esriSpatialRelIntersects&outFields=regions_local.Region_Agg,reference_gages.site_id,reference_gages.site_name,reference_gages.da_gis_mi2,reference_gages.lat_dd_nad,reference_gages.long_dd_na&returnGeometry=false&returnIdsOnly=false&returnCountOnly=false&returnZ=false&returnM=false&returnDistinctValues=false&f=pjson',
