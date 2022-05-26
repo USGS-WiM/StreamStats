a2b8f0f206d39535e8d4b48467792fa0d4e6168e
Wavra <hwavra@usgs.gov>
2022-05-26 11:14:27 -0500

    code cleanup

--- src/Controllers/ReportController.ts
+++ src/Controllers/ReportController.ts
@@ -672,7 +672,6 @@ module StreamStats.Controllers {
             if (!this.studyAreaService.selectedStudyArea) return;
             this.overlays = {};
             this.studyAreaService.selectedStudyArea.FeatureCollection.features.forEach((item) => {
-                console.log(item)
                 this.addGeoJSON(item.id, item);
             });
 
