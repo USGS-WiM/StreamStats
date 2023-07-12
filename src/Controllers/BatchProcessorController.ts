bb056e3b0339895de07f2c86e23249206d612590
amedenblik <amedenblik@usgs.gov>
2023-07-12 16:35:51 -0400

    #1651 undo merge to fix errors

--- src/Controllers/BatchProcessorController.ts
+++ src/Controllers/BatchProcessorController.ts
@@ -1,4 +1,4 @@
-﻿//------------------------------------------------------------------------------
+﻿﻿//------------------------------------------------------------------------------
 //----- BatchProcessorController ---------------------------------------------------------------
 //------------------------------------------------------------------------------
 
@@ -48,22 +48,6 @@ module StreamStats.Controllers {
         public description: string;
         public checked: boolean;
         public toggleable: boolean;
-    }
-
-    interface IStreamGrid {
-        region: string,
-        regionCode: string,
-        fileName: string,
-        downloadURL: string,
-        lastModified: Date;
-    }
-
-    class StreamGrid implements IStreamGrid {
-        public region: string;
-        public regionCode: string;
-        public fileName: string;
-        public downloadURL: string;
-        public lastModified: Date;
     }
 
     interface IBatchStatusMessage {
@@ -133,7 +117,7 @@ module StreamStats.Controllers {
         public internalHost: boolean;
 
         // Regions
-        public regionList: Array<any>;
+        public regionList: Object;
         public selectedRegion: string;
 
         // Flow Stats
@@ -172,10 +156,6 @@ module StreamStats.Controllers {
         public manageQueueList: Array<BatchStatus>;
         public retrievingManageQueue: boolean;
 
-        // stream grids
-        public streamGridList: Array<StreamGrid>;
-        public retrievingStreamGrids: boolean;
-
         //Constructor
         //-+-+-+-+-+-+-+-+-+-+-+-
         static $inject = ['$scope', '$http', 'StreamStats.Services.ModalService', 'StreamStats.Services.nssService', '$modalInstance', 'toaster'];
@@ -207,8 +187,6 @@ module StreamStats.Controllers {
             this.flowStatsListSpinner = false;
             this.parametersListSpinner = false;
             this.batchStatusMessageList = [];
-            this.streamGridList = [];
-            this.retrievingStreamGrids = false;
             this.batchStatusList = [];
             this.retrievingManageQueue = false;
             this.flowStatIDs = [];
@@ -294,6 +272,11 @@ module StreamStats.Controllers {
                     )
                 }
             );
+
+            
+
+
+
 
         }
 
@@ -529,6 +512,7 @@ module StreamStats.Controllers {
                     }
                 }
 
+
             });
 
             // toggle switch
@@ -732,6 +716,8 @@ module StreamStats.Controllers {
                     }
                 }).finally(() => {
                 });
+
+
         }
 
         // unpause a batch
@@ -751,15 +737,6 @@ module StreamStats.Controllers {
                     }
                 }).finally(() => {
                 });
-        }
-
-        public loadStreamGrids(): void {
-            this.getStreamGrids().then(
-                response => {
-                    this.streamGridList = response;
-                    this.retrievingStreamGrids = false;
-                }
-            );
         }
 
         // Service methods
@@ -959,44 +936,6 @@ module StreamStats.Controllers {
                     return response;
                 }, (error) => {
                     return error;
-                }).finally(() => {
-                });
-        }
-
-        // load stream grids
-        public getStreamGrids(): ng.IPromise<any> {
-            var url = configuration.baseurls['BatchProcessorServices'] + configuration.queryparams['SSBatchProcessorStreamGrids'];
-            var request: WiM.Services.Helpers.RequestInfo = new WiM.Services.Helpers.RequestInfo(url, true);
-
-            return this.Execute(request).then(
-                (response: any) => {
-
-                    var streamGrids = [];
-
-                    response.data.forEach((item) => {
-
-                        let baseURL = "https://s3.amazonaws.com/test.streamstats.usgs.gov/batch-processor/streamgrids/"
-                        if (window.location.host === 'streamstats.usgs.gov') {
-                            baseURL = "https://s3.amazonaws.com/streamstats.usgs.gov/batch-processor/streamgrids/"
-                        } 
-
-                        try {
-                            let streamGrid: StreamGrid = {
-                                region: this.regionList.filter((region) => {return region.Code == item.RegionCode})[0]["Name"],
-                                regionCode: item.RegionCode,
-                                fileName: item.FileName,
-                                downloadURL: baseURL + item.FileName,
-                                lastModified: item.LastModified
-                            }
-                            streamGrids.push(streamGrid);
-                        }
-
-                        catch (e) {
-                            console.log(e)
-                        }
-                    });
-                    return streamGrids;
-                }, (error) => {
                 }).finally(() => {
                 });
         }
